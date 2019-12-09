2562dc738510fcb4e5959460fe72c9d94329eaca
JohnMaple <934879001@qq.com>
2019-12-10 00:11:28 +0800

    发送短信验证码

--- app/Http/Controllers/Api/VerificationCodesController.php
+++ app/Http/Controllers/Api/VerificationCodesController.php
@@ -2,12 +2,47 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Http\Requests\Api\VerificationCodeRequest;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Str;
+use Overtrue\EasySms\EasySms;
 
 class VerificationCodesController extends Controller
 {
-    public function store()
+    public function store(VerificationCodeRequest $request,  EasySms $easySms)
     {
-        return response()->json(['test_message' => 'store verification code']);
+        $phone = $request->phone;
+
+        // 生成4位随机数，左侧补0
+        $code = str_pad(random_int(1, 9999), 4, 0, STR_PAD_LEFT);
+
+        // 区分测试和线上环境
+        if (!app()->environment('production')) {
+            $code = '1234';
+        } else {
+            try {
+                $request = $easySms->send($phone, [
+                    'template' => config('easysms.gateways.aliyun.templates.register'),
+                    'data' => [
+                        'code' => $code
+                    ],
+                ]);
+            } catch (\Overtrue\EasySms\Exceptions\NoGatewayAvailableException $exception) {
+                $message = $exception->getException('aliyun')->getMessage();
+                abort(500, $message ?: '短信发送异常');
+            }
+        }
+
+        // 返回一个随机字符串，用来请求别的接口，传统使用session来维持两个请求的关联性，但是接口是无状态，相互独立的，所以需要一个随机的key，key存在缓存中，可以设有效期
+        $key = 'verificationCode_'.Str::random(15);
+        $expiredAt = now()->addMinutes(5);
+        // 缓存验证码5分钟过期
+        Cache::put($key, ['phone' => $phone, 'code' => $code], $expiredAt);
+
+        return response()->json([
+            'key' => $key,
+            'expired_at' => $expiredAt->toDateTimeString(),
+        ])->setStatusCode(201);
     }
 }
