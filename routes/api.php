ce8bea9260c77bc8d7215a7c77697bcd2b7cb012
JohnMaple <934879001@qq.com>
2019-12-09 23:07:00 +0800

    API 基础配置

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,32 @@ use Illuminate\Http\Request;
 |
 */
 
+Route::prefix('v1')->name('api.v1.')->group(function() {
+    Route::get('version', function() {
+        return 'this is version v1';
+    })->name('version');
+});
+
+Route::prefix('v2')->name('api.v2.')->group(function() {
+    Route::get('version', function() {
+        return 'this is version v2';
+    })->name('version');
+});
+
+Route::prefix('v1')->name('api.v1.')->group(function() {
+    Route::get('version', function() {
+        abort(403, 'test');
+        return 'this is version v1';
+    })->name('version');
+});
+
+Route::prefix('v1')->namespace('Api')->name('api.v1.')->group(function () {
+    // 短信验证码
+    Route::post('verificationCodes', 'VerificationCodesController@store')
+        ->name('verificationCodes.store');
+});
+
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
